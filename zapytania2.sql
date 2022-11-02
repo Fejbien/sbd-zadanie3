a9a5c481a0fa4a43b347be96b275a6dfe406c772
Fabian <38659818+Fejbien@users.noreply.github.com>
2022-11-02 11:56:21 +0100

    3

--- zapytania2.sql
+++ zapytania2.sql
@@ -12,4 +12,4 @@ SELECT COUNT(*), AVG(`price`) FROM `products` WHERE `category_id`=1;
 
 SELECT SUM(`price`) FROM `products` WHERE `category_id`=2;
 
-SELECT MAX(`price`) FROM `products` WHERE `category_id`=1 or `category_id`=2 GROUP BY `category_id`;
+SELECT MAX(`price`) FROM `products` GROUP BY `category_id`;
---
+++ zapytania3.sql
@@ -0,0 +1,13 @@
+SELECT * FROM `products` WHERE `price` BETWEEN 10 AND 20;
+
+SELECT * FROM `products` WHERE `price` NOT BETWEEN 10 AND 20;
+
+SELECT * FROM `products` WHERE `category_id` IN (1, 2, 3);
+
+SELECT * FROM `orders` WHERE `shipper_id` IN (1, 2, 3);
+
+SELECT * FROM `products` WHERE `price` BETWEEN 10 AND 15 AND NOT `category_id` IN (1, 2, 3, 4);
+
+SELECT * FROM `orders` WHERE `order_date` BETWEEN '1996-7-01 00:00:00' AND '1996-7-31 00:00:00';
+
+SELECT * FROM `customers` WHERE `id` IN (SELECT `id` FROM `customers` WHERE `id` BETWEEN 77 AND 90);
