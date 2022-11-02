9ed7d746dc0a95131d9d8944d6ae2002c7986e83
fejbien <sucholasfejbien@gmail.com>
2022-11-02 09:54:12 +0100

    2

---
+++ zapytania2.sql
@@ -0,0 +1,15 @@
+SELECT MIN(`price`) FROM `products` WHERE 1;
+
+SELECT MAX(`price`) FROM `products` WHERE 1;
+
+SELECT count(*) FROM `products` WHERE 1;
+
+SELECT count(*) FROM `customers` WHERE 1;
+
+SELECT count(*) FROM `customers` GROUP BY `country`;
+
+SELECT COUNT(*), AVG(`price`) FROM `products` WHERE `category_id`=1;
+
+SELECT SUM(`price`) FROM `products` WHERE `category_id`=2;
+
+SELECT MAX(`price`) FROM `products` GROUP BY `category_id`;
