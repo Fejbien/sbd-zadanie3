25eee21da3ddede7249ad744b9e9b506a47db637
fejbien <sucholasfejbien@gmail.com>
2022-11-30 10:04:38 +0100

    skoncze perhaps?

--- zapytania10.sql
+++ zapytania10.sql
@@ -1,3 +1,5 @@
 // DISTINCT
 SELECT `country` FROM `customers` WHERE 1;
 SELECT DISTINCT(`country`) FROM `customers` WHERE 1;
+
+SELECT COUNT(a.d) as "Kraje bez powtorzen: " FROM (SELECT DISTINCT(`country`) as d FROM `customers` GROUP BY country HAVING COUNT(`country`) = 1) as a
